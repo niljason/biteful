00fe9a30930e68210aa638f87d85ca137351be6b
Perry Huang <perryhuang2015@gmail.com>
2026-03-10 18:43:15 -0400

    style: lint filters

--- backend/filters/LoginFilter.h
+++ backend/filters/LoginFilter.h
@@ -9,13 +9,9 @@
 #include <drogon/HttpFilter.h>
 using namespace drogon;
 
-
-class LoginFilter : public HttpFilter<LoginFilter>
-{
-  public:
-    LoginFilter() {}
-    void doFilter(const HttpRequestPtr &req,
-                  FilterCallback &&fcb,
-                  FilterChainCallback &&fccb) override;
+class LoginFilter : public HttpFilter<LoginFilter> {
+public:
+    LoginFilter() {
+    }
+    void doFilter(const HttpRequestPtr& req, FilterCallback&& fcb, FilterChainCallback&& fccb) override;
 };
-
