0f6244b6f0fd80e89f052888ba1571302c3bb12f
Perry Huang <perryhuang2015@gmail.com>
2026-03-19 11:23:09 -0400

    feat: fix type comparison in loginFilter

--- backend/filters/LoginFilter.cc
+++ backend/filters/LoginFilter.cc
@@ -13,7 +13,7 @@ using namespace drogon;
 
 void LoginFilter::doFilter(const HttpRequestPtr& req, FilterCallback&& fcb, FilterChainCallback&& fccb) {
     // check if session has a logged in variable and if that variable is true
-    if (req->session()->find("loggedIn") && req->session()->get<std::string>("loggedIn") != "true") {
+    if (req->session()->find("loggedIn") && req->session()->get<bool>("loggedIn") != true) {
         auto resp = drogon::HttpResponse::newHttpResponse();
         resp->setStatusCode(drogon::k400BadRequest);
         fcb(resp);
