6d3fadaef2ec706828924e6035ab6cea3fb87f91
Perry Huang <perryhuang2015@gmail.com>
2026-03-19 12:42:02 -0400

    feat: fix synchronization bug in dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,58 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+    createBrowserRouter,
+    redirect,
+    RouterProvider,
+} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import LoginForm from "./features/auth/components/LoginForm";
 import SignupForm from "./features/users/components/SignupForm";
 import Dashboard from "./features/users/components/Dashboard";
 
+const isAuthenticated = () => {
+    if (localStorage.getItem("sessionId") === null) {
+        throw redirect("/login");
+    }
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <MainLayout>
+                <Dashboard />
+            </MainLayout>
+        ),
+        middleware: [isAuthenticated],
+    },
+    {
+        path: "/login",
+        element: (
+            <MainLayout>
+                <LoginForm />
+            </MainLayout>
+        ),
+    },
+    {
+        path: "/signup",
+        element: (
+            <MainLayout>
+                <SignupForm />
+            </MainLayout>
+        ),
+    },
+    {
+        path: "/dashboard",
+        element: (
+            <MainLayout>
+                <Dashboard />
+            </MainLayout>
+        ),
+        middleware: [isAuthenticated],
+    },
+]);
+
 function App() {
-    // check if session id exists
-    const isAuthenticated = () => {
-        return localStorage.getItem("sessionId") !== null;
-    };
-
-    isAuthenticated();
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<MainLayout />}>
-                    {/* Public Routes */}
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/signup" element={<SignupForm />} />
-
-                    {/* Protected Route */}
-                    <Route
-                        path="/dashboard"
-                        element={
-                            isAuthenticated() ? (
-                                <Dashboard />
-                            ) : (
-                                <Navigate to="/login" />
-                            )
-                        }
-                    />
-
-                    {/* Default Route */}
-                    <Route
-                        path="/"
-                        element={
-                            <Navigate
-                                to={isAuthenticated() ? "/dashboard" : "/login"}
-                            />
-                        }
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
