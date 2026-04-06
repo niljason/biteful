70e9ada1acdd70407944a645968825b61707731f
Jason Lin <8333jlin@gmail.com>
2026-04-06 10:36:51 -0400

    feat: controllers for FoodPantry

---
+++ backend/controllers/FoodPantry.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <drogon/HttpController.h>
+
+namespace biteful {
+namespace controllers {
+class FoodPantry : public drogon::HttpController<FoodPantry> {
+public:
+    METHOD_LIST_BEGIN
+    ADD_METHOD_TO(FoodPantry::getPantryById, "/pantries/{id}", drogon::Get);
+    ADD_METHOD_TO(FoodPantry::getAllPantries, "/pantries", drogon::Get);
+    METHOD_LIST_END
+
+    void getPantryById(const drogon::HttpRequestPtr& req,
+                       std::function<void(const drogon::HttpResponsePtr&)>&& callback, int id);
+    void getAllPantries(const drogon::HttpRequestPtr& req,
+                        std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+};
+}  // namespace controllers
+}  // namespace biteful
