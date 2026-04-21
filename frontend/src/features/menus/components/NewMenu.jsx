51d0e952ea34fc20fde6e4a42f2c154dd25cb2ee
Perry Huang <perryhuang2015@gmail.com>
2026-04-21 12:07:22 -0400

    feat: add comments for later

--- frontend/src/features/menus/components/NewMenu.jsx
+++ frontend/src/features/menus/components/NewMenu.jsx
@@ -4,16 +4,23 @@ const NewMenu = ({ restaurantId, menuData }) => {
   const items = [];
   for (const item of menuData["menu_items"]) {
     items.push(
+      // make each of these an input with default inputs as the text
+      // put that stuff in fooditem
       <p key={item["dish_name"]}>
         {item["dish_name"]}: {item["dish_price"]}
       </p>,
     );
   }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // submit the current info held in the form
+  };
   return (
     <>
       <p>This is restaurant {restaurantId}</p>
       {menuData ? <p>There is data</p> : <p>There is no data...</p>}
       {items}
+      <form action="POST" onSubmit={onSubmit}></form>
       <p>This is a work in progress</p>
     </>
   );
